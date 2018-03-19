625de49d0b6f244d179cda1138bea3a7a0fd8427
kdoddy <tuc36615@temple.edu>
2018-03-19 12:46:23 -0700

    lvl 1.1 challenge + coderunner cleanup

--- coderunner/coderunner.js
+++ coderunner/coderunner.js
@@ -2,11 +2,9 @@ const Sandbox = require('sandbox');
 var s = new Sandbox();
 
 var runThis = function(userCode, tests) {
-  console.log('TESTS in runthis are', tests)
-  var masterTests = tests; // need hardcoded tests here for initial test
+  var masterTests = tests;
   return new Promise(resolve => { 
     s.run(`${userCode} ${masterTests};`, function(output) {
-      console.log('output in runthis = ', output.result);
       resolve(output.result);
     })
   });
