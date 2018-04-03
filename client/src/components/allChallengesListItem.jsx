57af6b4d18fd898198ee10cac9fd2b6fb336302c
kcloud99 <kylemcleod1@gmail.com>
2018-04-02 20:01:43 -0700

    Trying to fix bug in allChallengesListItem

--- client/src/components/allChallengesListItem.jsx
+++ client/src/components/allChallengesListItem.jsx
@@ -3,15 +3,15 @@ import {Grid, Button, Icon, Label} from 'semantic-ui-react';
 
 const AllChallengesListItem = (props) => {
   const {challenge, user} = props;
-  const color;
+  const labelColor = '';
   if (challenge.level === 1) {
-    color = 'teal';
+    labelColor = 'teal';
   } else if (challenge.level === 2 || challenge.level === 3) {
-    color = 'yellow';
+    labelColor = 'yellow';
   } else {
-    color = 'red';
+    labelColor = 'red';
   }
-  const showSolutions;
+  const showSolutions = '';
   if (user.completedChallenges.indexOf(challenge.name) !== -1) {
     showSolutions = (<Button animated='vertical'>
       <Button.Content hidden>Solutions</Button.Content>
@@ -22,24 +22,20 @@ const AllChallengesListItem = (props) => {
   } else {
     showSolutions = <Icon name='remove' color='red' />;
   }
-  return(
-    <Grid.Row key={challenge.name}>
+  return <Grid.Row key={challenge.name}>
       <Grid.Column width={10}>
         <Link to=`/allchallenges/${challenge.name}`>{challenge.name}</Link>
       </Grid.Column>
       <Grid.Column width={2}>
-        <Label color={color}>
-          {challenge.level}
+        <Label color={color}>{challenge.level}</Label>
+      </Grid.Column>
+      <Grid.Column width={2}>{showSolutions}</Grid.Column>
+      <Grid.Column width={2}>
+        <Label color="blue" tag>
+          {challenge.ranking}
         </Label>
       </Grid.Column>
-      <Grid.Column width={2}>
-        {showSolutions}
-      </Grid.Column>
-      <Grid.Column width={2}>
-        <Label color='blue' tag>{challenge.ranking}</Label>
-      </Grid.Column>
-    </Grid.Row>
-  );
+    </Grid.Row>;
 }
 
 export default AllChallengesListItem;
