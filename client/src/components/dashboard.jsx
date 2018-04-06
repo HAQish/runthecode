cceba6dc80e00c60436819c70461a51db969cd5d
kcloud99 <kylemcleod1@gmail.com>
2018-04-06 01:07:14 -0700

    Fix styling on login button, work on home screen style,  Dashboard style

--- client/src/components/dashboard.jsx
+++ client/src/components/dashboard.jsx
@@ -37,7 +37,12 @@ const Heading = styled.h1`
   text-align: center;
   padding-bottom: 20px;
   font-size: 5rem;
-  color: #4CBB6B;
+  color: #6f999d;
+`;
+
+const SubHeading = styled.h1`
+  color: #9ab751;
+  font-size: 3rem;
 `;
 
 class Dashboard extends React.Component {
@@ -107,7 +112,7 @@ class Dashboard extends React.Component {
             <Grid.Column width={8}>
               <Card centered raised color="blue" style={{ width: "70%", minHeight: "40vh" }}>
                 <Card.Content>
-                  <Card.Header><h1>Your Learning Path</h1></Card.Header>
+                  <Card.Header><SubHeading>Your Learning Path</SubHeading></Card.Header>
                 </Card.Content>
                 <Card.Content>
                   <br />
@@ -148,7 +153,7 @@ class Dashboard extends React.Component {
             <Grid.Column width={8}>
               <Card centered raised color="blue" style={{ width: "70%", minHeight: "40vh" }}>
                 <Card.Content>
-                  <Card.Header><h1>Your Challenge Path</h1></Card.Header>
+                  <Card.Header><SubHeading>Your Challenge Path</SubHeading></Card.Header>
                 </Card.Content>
                 <Card.Content>
                   <Feed>
--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -76,96 +76,119 @@ class Home extends React.Component {
   render() {
     const { prompt, challengeName, masterTestDescriptions } = this.state.starterChallenge;
     const { masterSolutionCode } = this.state;
-    return(
-      <div style={{marginTop: '0px'}} >
-      <div className='homepage banner'>
-        <Header icon inverted textAlign='center' size='huge' style={{paddingTop: '20px'}}>
-          <Icon name='code' size='big' inverted circular />
-          <Header.Content>
-            LevelUP Code
-          </Header.Content>
-        </Header>
-        <div className='parent'>
-          <Image className='monitor' src='images/monitor.png' />
-          <div className='editor'>
-          <AceEditor
-            className='editor'
-            mode='javascript'
-            theme="kuroir"
-            onChange={this.onChange}
-            value={masterSolutionCode}
-            editorProps={{ $blockScrolling: true }}
-            width='100%'
-            height='100%'
-          />
-          <Button onClick={this.onSubmitToServer} primary content='Start your journey' style={{float: 'left', marginBottom: '10px'}}/>
+    return <div style={{ marginTop: "0px" }}>
+        <div className="homepage banner">
+          <Header icon inverted textAlign="center" size="huge" style={{ paddingTop: "20px" }}>
+            <Icon name="code" size="big" inverted circular />
+            <Header.Content>LevelUP Code</Header.Content>
+          </Header>
+          <div className="parent">
+            <Image className="monitor" src="images/monitor.png" />
+            <div className="editor">
+              <AceEditor className="editor" mode="javascript" theme="kuroir" onChange={this.onChange} value={masterSolutionCode} editorProps={{ $blockScrolling: true }} width="100%" height="100%" />
+              <Button onClick={this.onSubmitToServer} primary content="Start your journey" style={{ float: "left", marginBottom: "10px" }} />
+            </div>
           </div>
+          <Modal style={{ height: "65%" }} basic dimmer style={{ height: "80%" }} closeOnDimmerClick open={this.state.openModal} onClose={this.closeModal}>
+            <Header icon="signup" content="Signup Page" />
+            <Modal.Content>
+              <Modal.Description>
+                <Header inverted>
+                  Get Ready for a coding experience like no other
+                </Header>
+                <Signup handleLogin={this.handleLoginSubmit} />
+              </Modal.Description>
+            </Modal.Content>
+            <Modal.Actions>
+              <Button color="red" onClick={this.closeSignupModal}>
+                <Icon name="remove" /> Close
+              </Button>
+            </Modal.Actions>
+          </Modal>
         </div>
-        <Modal
-          style={{ height: '65%' }}
-          basic
-          dimmer
-          style={{ height: "80%" }}
-          closeOnDimmerClick
-          open={this.state.openModal}
-          onClose={this.closeModal}>
-          <Header icon='signup' content='Signup Page' />
-          <Modal.Content>
-            <Modal.Description>
-              <Header inverted>Get Ready for a coding experience like no other</Header>
-              <Signup handleLogin={this.handleLoginSubmit} />
-            </Modal.Description>
-          </Modal.Content>
-          <Modal.Actions >
-            <Button color='red' onClick={this.closeSignupModal}>
-              <Icon name='remove' /> Close
-            </Button>
-          </Modal.Actions>
-        </Modal>
-      </div>
-        <Grid columns={2} relaxed textAlign='center' divided='vertically' style={{padding: '30px'}}>
-          <Grid.Row>
+        <div className="marketing">
+          <Header as="h1">Welcome to LevelUp Code</Header>
+          <Header.Subheader as="h2">
+            Join us on an adventure in coding!
+          </Header.Subheader>
+          <Header.Subheader as="h3">
+            Using our unique collaborative platform, you not only get a
+            chance to practice your programming skills in a fun environment.
+            You also get to enjoy all the benefits of pair programming, the
+            collaborative experience necessary for any good software
+            engineer!
+          </Header.Subheader>
+          <Header.Subheader as="h3">
+            Take your skills to the next level by yourself or with a friend... You can even meet like minded
+            future programmers right on the site! So what are you waiting for? Fill out our first test above and signup
+            for an adventure in the wonderful world of software engineering!
+          </Header.Subheader>
+        </div>
+        <Grid columns={2} relaxed textAlign="center" divided="vertically" style={{ padding: "30px" }} className="about-us">
+          <Grid.Row className="about-us">
             <Grid.Column width={10}>
               <Container text>
                 <Segment raised>
-                  <Header as='h2'>Kevin Doddy - Fullstack Assassin</Header>
-                  <p>Hi I'm Kevin, the Fullstack Assassin...</p>
+                  <Header as="h2">
+                    Hi, I'm Kevin, the Fullstack Assassin...
+                  </Header>
+                  <p>
+                    I enjoy long walks on the beach and CODING SOME AMAZING
+                    PROBLEMS FOR LEVELUPCODE!!! Pennsylvania born and
+                    raised, and have a true passion for creating a safe and
+                    fun place for practicing your programming skills.
+                  </p>
                 </Segment>
               </Container>
             </Grid.Column>
             <Grid.Column width={6}>
-              <Image src='images/code_assassin.jpg' size='medium' bordered />
+              <Image src="images/code_assassin.jpg" size="medium" circular />
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row className="about-us-list">
+            <Grid.Column width={6}>
+              <Image src="images/ninja-coder.png" circular size="medium" style={{ marginLeft: "100px" }} />
+            </Grid.Column>
+            <Grid.Column width={10}>
+              <Container text>
+                <Segment raised>
+                  <Header as="h2">
+                    Hi I'm Habib, the Backend Ninja...
+                  </Header>
+                  <p>
+                    I love nothing more than pluggin in my headphones and
+                    jamming out to a good coding session! I hope you all
+                    enjoy the website, and really take advantage of the
+                    great pair programming opportunites this community
+                    provides!
+                  </p>
+                </Segment>
+              </Container>
             </Grid.Column>
           </Grid.Row>
           <Grid.Row>
-            <Grid.Column width={6}>
-              <Image src='images/ninja-coder.png' size='medium' bordered style={{marginLeft: '100px'}} />
-            </Grid.Column>
             <Grid.Column width={10}>
               <Container text>
                 <Segment raised>
-                  <Header as='h2'>Habib Qureshi - Backend Ninja</Header>
-                  <p>Hi I'm Habib, the Backend Ninja...</p>
-                </Segment>
-              </Container>
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row>
-            <Grid.Column width={10}>
-              <Container text>
-                <Segment raised>
-                  <Header as='h2'>Kyle McLeod - Frontend Wizard</Header>
-                  <p>Hi I'm Kyle, the Frontend Wizard...</p>
+                  <Header as="h2">
+                    Hi I'm Kyle, the Frontend Wizard...
+                  </Header>
+                  <p>
+                    Originally from Seattle, WA, I now spend my time roaming
+                    the wilds, helping young programmers like yourself find
+                    a path through the wilderness... Follow me as we journey
+                    into the world of programming, I promise it will be a
+                    blast!
+                  </p>
                 </Segment>
               </Container>
             </Grid.Column>
             <Grid.Column width={6}>
-              <Image src='images/code_wizard.png' size='medium' bordered />
+              <Image src="images/code_wizard.png" size="medium" />
             </Grid.Column>
           </Grid.Row>
         </Grid>
-      </div>
-    )
+      </div>;
   }
 }
 
